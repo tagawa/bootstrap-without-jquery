48115cc5d06e883899b82f414b1c9f0e3dc1d801
Daniel Davis <github@kyokodaniel.com>
2014-07-04 20:45:26 +0900

    Fixed version number, moved files to sub-directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,14 +35,9 @@ module.exports = function(grunt) {
     });
     
     //grunt.loadNpmTasks('grunt-contrib-concat');
-    //grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    //grunt.loadNpmTasks('grunt-contrib-watch');
-    //grunt.loadNpmTasks('grunt-autoprefixer');
 
-    // Default task.
-    //grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'autoprefixer', 'cssmin']);
     grunt.registerTask('default', ['jshint', 'uglify']);
 };
 
